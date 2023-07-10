a51fb574f5b20b2f27d63f065cde5b6244368f6d
LuLu <lu.luzan@gmail.com>
2023-07-10 14:16:13 +0200

    19 passed

--- src/saxon/Saxon.js
+++ src/saxon/Saxon.js
@@ -1,3 +1,21 @@
-class Saxon {}
+const Soldier = require("../soldier/Soldier.js");
+
+class Saxon extends Soldier {
+    constructor(health, strength) {
+    super(health, strength);
+    }
+    attack(){
+        return(this.strength);
+    }
+    receiveDamage(damage){
+        this.health -=damage;
+        if(health >0){
+            return 'A Saxon has received ${damage} points of damage';
+        }else{
+            return 'A Saxon has dies in combat' ;
+        }
+    }
+
+}
 
 module.exports = Saxon;
